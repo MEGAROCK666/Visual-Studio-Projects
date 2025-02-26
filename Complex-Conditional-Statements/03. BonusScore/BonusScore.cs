4cc9ebc6815c8b0614abd64dba1fbc6fd28a3107
Nikolay Kostov <132822784+MEGAROCK666@users.noreply.github.com>
2025-02-26 21:06:31 +0200

    Add files via upload

---
+++ Complex-Conditional-Statements/03. BonusScore/BonusScore.cs
@@ -0,0 +1,23 @@
+﻿namespace _03._BonusScore
+{
+    internal class BonusScore
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+
+            if (num1 >= 0 && num1 <= 3) 
+            {
+                Console.WriteLine(num1 + 5);
+            }
+            else if (num1 >= 4 && num1 <= 6)
+            {
+                Console.WriteLine(num1 + 15);
+            }
+            else if (num1 >= 7 && num1 <= 9)
+            {
+                Console.WriteLine(num1 + 20);
+            }
+        }
+    }
+}
---
+++ Complex-Conditional-Statements/04. FoodOrDrink/FoodOrDrink.cs
@@ -0,0 +1,51 @@
+﻿namespace _04._FoodOrDrink
+{
+    internal class FoodOrDrink
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+
+            if (type == "curry")
+            {
+                Console.WriteLine("food");
+            }
+            else if (type == "noodles")
+            {
+                Console.WriteLine("food");
+            }
+            else if (type == "sushi")
+            {
+                Console.WriteLine("food");
+            }
+            else if (type == "spaghetti")
+            {
+                Console.WriteLine("food");
+            }
+            else if (type == "bread")
+            {
+                Console.WriteLine("food");
+            }
+            else if (type == "tea")
+            {
+                Console.WriteLine("drink");
+            }
+            else if (type == "water")
+            {
+                Console.WriteLine("drink");
+            }
+            else if (type == "coffee")
+            {
+                Console.WriteLine("drink");
+            }
+            else if (type == "juice")
+            {
+                Console.WriteLine("drink");
+            }
+            else 
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
