f66a982446e184866a106ddeb5d06670fc313832
Nikolay Kostov <132822784+MEGAROCK666@users.noreply.github.com>
2025-02-26 21:07:38 +0200

    Add files via upload

---
+++ Complex-Conditional-Statements/05. FruitOrVegetable/FruitOrVegetable.cs
@@ -0,0 +1,47 @@
+﻿namespace _05._FruitOrVegetable
+{
+    internal class FruitOrVegetable
+    {
+        static void Main(string[] args)
+        {
+                string type = Console.ReadLine();
+
+                if (type == "banana")
+                {
+                    Console.WriteLine("fruit");
+                }
+                else if (type == "apple")
+                {
+                    Console.WriteLine("fruit");
+                }
+                else if (type == "kiwi")
+                {
+                    Console.WriteLine("fruit");
+                }
+                else if (type == "cherry")
+                {
+                    Console.WriteLine("fruit");
+                }
+                else if (type == "lemon")
+                {
+                    Console.WriteLine("fruit");
+                }
+                else if (type == "cucumber")
+                {
+                    Console.WriteLine("vegetable");
+                }
+                else if (type == "pepper")
+                {
+                    Console.WriteLine("vegetable");
+                }
+                else if (type == "carrot")
+                {
+                    Console.WriteLine("vegetable");
+                }
+                else
+                {
+                    Console.WriteLine("unknown");
+                }
+            }
+        }
+    }
